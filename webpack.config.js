0be30a8fb2c91252c6caa960ba9b167489b38686
Rishat Muhametshin <rishatmuhametshin@gmail.com>
2016-07-19 18:50:15 +0400

    more configs

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var path = require('path');
+var fs = require('fs');
+var externals = {};
+
+fs.readdirSync('node_modules').filter(function (x) {
+  return ['.bin'].indexOf(x) === -1;
+}).forEach(function (mod) {
+  externals[mod] = 'commonjs ' + mod;
+});
+
+module.exports = {
+  entry: {
+    index: './src/index.js'
+  },
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'index.js'
+  },
+  target: 'node',
+  module: {
+    loaders: [{
+      loader: 'babel-loader',
+      include: [path.resolve(__dirname, 'src')],
+      test: /\.js$/,
+      query: {
+        presets: ['es2015', 'stage-0']
+      }
+    }]
+  },
+  externals: externals
+};
